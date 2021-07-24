a185f7b2dc70138f1210b61b2f7b4d70abf56c34
gethziyaljoyce <87714086+gethziyaljoyce@users.noreply.github.com>
2021-07-24 10:43:25 +0530

    Add files via upload

---
+++ resumeJSON.js
@@ -0,0 +1,78 @@
+var resume = {
+    Name: "Gethziyaljoyce",
+    Mobile: 9600828943,
+    mailId: "jc.getjoy@gmail.com",
+    careerObjectives: "To use my skills in the best possible way for achieving the company's goal",
+    EduQualification: [
+        {
+            coursedegree: "BE/EEE",
+            nameOfInst: "Kalasalingam Institute Of Technology",
+            BoardUniv: "Anna university",
+            yearOfPassing: 2014,
+            percentagecgpa: 8.14
+        },
+        {
+            coursedegree: "HSC",
+            nameOfInst: "PKN girls hr sec school",
+            BoardUniv: "State board",
+            yearOfPassing: 2010,
+            percentagecgpa: 87.4
+        },
+        {
+            coursedegree: "SSLC",
+            nameOfInst: "Govt girls hr sec school",
+            BoardUniv: "state board",
+            yearOfPassing: 2008,
+            percentagecgpa: 95.4
+        }
+    ],
+    IndusrialExposure: [
+        {
+            Industry: "mcnold electrical equipments(p)ltd madurai",
+            learning: "construction and testing of transformer",
+            Duration: "19.12.11-23.12.11"
+        },
+        {
+            Industry: "Tuticorin thermal power station, tuticorin",
+            learning: "Functions of power generation unit and transmission unit",
+            Duration: "10.12.12 - 14.12.12 "
+        },
+
+        {
+            Industry: "Zeal soft technology solutions,madurai",
+            learning: "Basics about micro processor and controller",
+            Duration: " 5.6.13 - 28.6.13 "
+        }
+    ],
+    projectTitle: "An integreated healthcare system of patient monitering in hospital environment using wireless network",
+    cocurricularActivites: [
+        {
+            title: "nationalconference",
+            place: "at sedhu institute of technology,virudhunagar"
+        },
+        {
+            title: "internationalconference",
+            place: "At NPR college of engineering and technology,natham"
+        },
+        {
+            title: "paperpresentation",
+            place: "At national engineering college,kovilpatti"
+        },
+        {
+            title: "workshop1-zonal rounds of robo tyrst-2013",
+            place: "KLN college of engineering,madurai"
+        },
+        {
+            title: "workshop2-printed circuit boards",
+            place: "kalasalingam institute of technology,krishnankoil"
+        },
+    ],
+    personalInformation:
+    {
+        fathername: "A.P.Marichamy",
+        gender: "female",
+        dob: "15.12.92",
+        address: "65,zion nagar,tirumangalam,madurai"
+    },
+    declaration: "I solemnly declare that the above furnished information is true to the best of my knowledge and i will be attentive to the work given to me"
+}
